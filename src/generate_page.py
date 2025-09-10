20b0a1226d6ff9b0402fe0761c6d0ed2950a17e4
leprinx <pep.arrufat@gmail.com>
2025-09-10 15:22:57 +0200

    E:Add to GitHub pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
             return re.sub(r"^# ", "", line.strip())
     raise Exception("No h1 was found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as my_file:
         md = my_file.read()
@@ -20,23 +20,27 @@ def generate_page(from_path, template_path, dest_path):
     html = html_node.to_html()
     title = extract_title(md)
     full_html = re.sub("{{ Title }}", title, template_text)
-    full_html = re.sub("{{ Content }}", html, template_text)
+    full_html = re.sub("{{ Content }}", html, full_html)
+    full_html = full_html.replace("href='/", f"href='{basepath}")
+    full_html = full_html.replace("src='/", f"src='{basepath}")
     dest_dir = os.path.dirname(dest_path)
     if dest_dir and not os.path.exists(dest_dir):
         os.makedirs(dest_path)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(full_html)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
-    if not os.path.exists(dir_path_content):
-        raise Exception("no content path found")
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path)
     content_path_items = os.listdir(dir_path_content)
     for item in content_path_items:
-        if os.path.isfile(item) and item.endswith(".md"):
+        if os.path.isfile(os.path.join(dir_path_content, item)) and item.endswith(".md"):
+            new_item_ext = item.replace(".md", ".html")
+            new_dest = os.path.join(dest_dir_path, new_item_ext)
+            generate_page(os.path.join(dir_path_content, item), template_path, new_dest, basepath)
+        if os.path.isdir(os.path.join(dir_path_content, item)):
             new_dest = os.path.join(dest_dir_path, item)
-            generate_page(os.path.join(dir_path_content, item), template_path, new_dest)
-        elif os.path.isdir(item):
-            generate_page_recursive(os.path.join(dir_path_content, item), template_path, dest_dir_path)
+            generate_page_recursive(os.path.join(dir_path_content, item), template_path, new_dest, basepath)
         
         
     
--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from textnode import TextNode
 from file_functions import copy_content_from_dir
-from generate_page import generate_page_recursive
+from generate_page import generate_page_recursive, generate_page
+import sys
 
 def main():
+    if len(sys.argv) < 2:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
     copy_content_from_dir("static", "public")
-    generate_page_recursive("content", "template.html", "public/index.html")
+    generate_page_recursive("content", "template.html", "docs", basepath)
 
 if __name__ == "__main__":
     main()
